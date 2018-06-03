8790d760c829ea70cd304c8dd513501b1eb1b6a1
mayuran siva <smayuran81@gmail.com>
2018-06-03 21:21:01 +0100

    gettingStarted

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 import { AppComponent } from './app/app.component';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; // This will help to compile it in the browser
 
 
 
 /**
- * 
- * 
+ 1.  We have the app Component
+ * 2. We register this as an app-root
+ * 3. Place it in HTML file
+ * 4. To boot strap import the app component and register it as declaration in NgModule
+ * 5. Also tell the Ngmodule to boot strap the bomponent
+ * 6. In the  main.ts file bootstrap the AppModule , by issuing the command below
  */
 import { AppModule } from './app/app.module';
 
